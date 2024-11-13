2e378476b3871ff3d0c3a4ef9bef5c879128e1f3
VK-Garg7 <14garg04@gmail.com>
2024-11-13 14:39:22 +0530

    store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./components/Header"
+import { Provider } from 'react-redux'
+import AppStore from './uti;s/AppStore'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>  
-    </>
+    <Provider store={AppStore}>
+      <div className='top'>
+        <Header />
+      </div>  
+    </Provider>
   )
 }
 

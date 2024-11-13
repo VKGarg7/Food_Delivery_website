c5db69932d7a0a8131e74ec0c78c5d71d9ef17bc
VK-Garg7 <14garg04@gmail.com>
2024-11-13 15:56:28 +0530

    config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,17 @@ export default {
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
+
+    darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('https://media.cntraveler.com/photos/61e886f19580ae68b5219023/master/pass/Oji%20Seichi%20Toronto_Oji%20Food-Food%20Spread_Roberto%20Caruso.jpg')",
+        'hero2':"url('https://wallpapers.com/images/featured/food-photography-background-iyvy1e8yztvry3g8.jpg')"
+      }
+    },
   },
   plugins: [],
+  prefix: 'tw-',
 }
 
